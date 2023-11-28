6ca4e6267a844473e435429e80c63b8f69c85d13
Andres Manzano <andres.manzano@empowermentlabs.co>
2023-11-28 11:53:17 -0500

    ..

--- src/components/canva/index.ts
+++ src/components/canva/index.ts
@@ -1,16 +1,25 @@
 import { Router, Express } from "express";
+import basicAuth from "express-basic-auth";
 import Canva from "./controller/canvaController";
 
 import { getTokenFromQueryString } from "../../utils/jwt_middleware/jwt_middleware";
 import { createJwtMiddleware } from "../../utils/jwt_middleware";
 
 const APP_ID = 'AAF0HNITXzs';
+const USERNAME = "username";
+const PASSWORD = "password";
 export default (app: Express) => {
   let canva = new Canva();
   const rt: Router = Router();
   app.use('/canva', rt);
 
   rt.get('/configuration/start', canva.start);
-  rt.get('/redirect-url', createJwtMiddleware(APP_ID, getTokenFromQueryString), canva.redirect)
+  rt.get('/redirect-url', 
+    createJwtMiddleware(APP_ID, getTokenFromQueryString),
+    basicAuth({
+      users: { [USERNAME]: PASSWORD },
+      challenge: true,
+    }), 
+    canva.redirect)
 
 }
