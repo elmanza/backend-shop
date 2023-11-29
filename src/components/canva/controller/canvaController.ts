9d551ff193eb859e3292e105ab108ce8566fce39
Andres Manzano <andres.manzano@empowermentlabs.co>
2023-11-28 22:41:42 -0500

    //

--- src/components/canva/controller/canvaController.ts
+++ src/components/canva/controller/canvaController.ts
@@ -6,15 +6,10 @@ export default class Canva {
   static COOKIE_EXPIRY_MS = 60 * 60 * 1000; // 5 minutes
   static CANVA_BASE_URL = "https://canva.com";
 
-  // example : canva/configuration/start?user=ASHTGpv-oqkfdrkDbEaB2UxgbHf2lSlx-rQpkr27u4c%3D&brand=ASHTGpu7ZK1edWZ60fsNDbEL1kwSmimJALkK-Ft78wI%3D&time=1701192163&extensions=API_NEXT&state=NHZIS2OcXbfj-z_tsHslcF92PQUHRm_k3_eni5O40yliP4a0taWhXZFuxcVBFxm060TWFk0qUcXQnNp8lKHdThoK5SuPzRwSzo49x90YE4UPa6mn4y9IBNFlBeDKUe89KUApzUjTTCKoXhHinemZ0CF_8IV2iU_Zlwtytg4LMtREia5f93jJczPS3bhu1bdhaNMi7hfljeBylu_T-tPNgaeVGQcmBUIC_40yqZTZa__k8KUW&canva_user_token=eyJraWQiOiI0ZDA3ZDMxZi1lYjFlLTRmMTQtODBlYy1jZDg4MWRlYmVkOTAiLCJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3MDExOTIxNjMsIm5iZiI6MTcwMTE5MjE2MywiZXhwIjoxNzAxMTkyNDYzLCJhdWQiOiJBQUYwSE5JVFh6cyIsInVzZXJJZCI6IkFTSFRHcHYtb3FrZmRya0RiRWFCMlV4Z2JIZjJsU2x4LXJRcGtyMjd1NGM9IiwiYnJhbmRJZCI6IkFTSFRHcHU3WksxZWRXWjYwZnNORGJFTDFrd1NtaW1KQUxrSy1GdDc4d0k9In0.lbz_iOBqqgmFYQPPoURNfQrB4unoYXpU_yAcFDqultw0FlkCOKQ-h9PAGWeDfXkc77HqWFeQb0rztEIYH_Bp4ooke4xpWzT3i_lu8EriQ40ga4WIyGSAxxRwZzewxyq3KuJRWQHpwtiOklL28Vasj4lvZ8gqyg1Gs7PIY0x7KTuNKQEM978_sTDCAPXInyv2w8aJLaEnxZ5DjMrdRWKj6lSWbYDm-krVZdlyM75Var5nBLY1QiLB2EsS86wFbLO5U2QMUrjn4fLJEr_WCikPNC4Q7esGxVGeLKEPMSUxeaSCSUH2CKNoX4iivmBwfZFQMe_ff4Cauj2nLC4tyyOnjQ&signatures=a96fcba2e15e5763a6b96d1fb89fc59a12c0a5c87262e98db6d959ab95facd8a&nonce=24aa9efd-48a6-4e41-ae8c-902e36e3f531
-
-  // https://www.canva.com/apps/configure/link?nonce=2f05f75b-5540-4dc7-92f5-3b29480e391f&state=NHZIS2OcXbfj-z_tsHslcF92PQUHRm_k3_eni5O40yliP4a0taWhXZFuxcVBFxm060TWFk0qUcXQnNp8lKHdThoK5SuPzRwSzo49x90YE4UPa6mn4y9IBNFlBeDKUe89KUApzUjTTCKoXhHinemZ0CF_8IV2iU_Zlwtytg4LMtREia5f93jJczPS3bhu1bdhaNMi7hfljeBylu_T-tPNgaeVGQcmBUIC_40yqZTZa__k8KUW
   async start(req: Request, res: Response, next: NextFunction){
     try {
-      // const { state } = req.query;
-      const stateParam: string = req?.query?.state?.toString() || "";
 
-      console.log({stateParam});
+      const stateParam: string = req?.query?.state?.toString() || "";
       let [nonceWithExpiry, nonce] = await canvaService.start(Canva.COOKIE_EXPIRY_MS);
       res.cookie("nonce", nonceWithExpiry, {
         secure: true,
@@ -24,42 +19,27 @@ export default class Canva {
       });
 
       const params = new URLSearchParams({
-        success: "true",
         nonce,
         state: stateParam,
       });
-      // res.json({response: `${Canva.CANVA_BASE_URL}/apps/configured?${params}`})
-      console.log("Desde mi endpoint [start] -> ", `https://www.canva.com/apps/configure/link?${params}`);
-      res.redirect(302, `https://www.canva.com/apps/configure/link?${params}`);
-      // res.json({url: `https://www.canva.com/apps/configure/link?${params}`})
+
+      res.redirect(302, `${Canva.CANVA_BASE_URL}/apps/configure/link?${params}`);
     } catch (error) {
       console.log(error);
       res.json({response: false})
     }
   }
-// https://backend-shop-production.up.railway.app/configuration/start?state=w5hH69-MLjPM1VtJAk079zs5wOjrr98eeZYh09p9tP_JfIAWrQyH0y0LwN5Kr6iuBU4-G48KXNWMo9FtKJHJOdyVECXTEDdG1pOK3lhtqLP-zTEq6WzZ7UU2XBH7Pmrb6ZcN5Nc6FQBW1n6UfNdAMg5221pBwn-Z5WWdqKnfpvG7GYNHE5OdWe7r0RaXaOWcwWFB5BbP7_aFZmyQJBrdue5_fTP4gUsXpjMkGq-hmQ8Ur6Y0
 
-// http://localhost:3001/canva/configuration/start?state=w5hH69-MLjPM1VtJAk079zs5wOjrr98eeZYh09p9tP_JfIAWrQyH0y0LwN5Kr6iuBU4-G48KXNWMo9FtKJHJOdyVECXTEDdG1pOK3lhtqLP-zTEq6WzZ7UU2XBH7Pmrb6ZcN5Nc6FQBW1n6UfNdAMg5221pBwn-Z5WWdqKnfpvG7GYNHE5OdWe7r0RaXaOWcwWFB5BbP7_aFZmyQJBrdue5_fTP4gUsXpjMkGq-hmQ8Ur6Y0
   async redirect(req: Request, res: Response, next: NextFunction){
     try {
-      // const nonceQuery = req.query.nonce;
-      console.log("Mis llaves autenticadas --> req.signedCookies --> ", req.signedCookies);
-
-      console.log("Información de la solicitud: --> ", {
-        method: req.method,
-        originalUrl: req.originalUrl,
-        params: req.params,
-        query: req.query,
-        body: req.body,
-        headers: req.headers,
-      });
 
       const failureResponse = () => {
         const params = new URLSearchParams({
           success: "false",
           state: req.query.state?.toString() || "",
         });
-        res.redirect(302, `${Canva.CANVA_BASE_URL}/apps/configured?${params}`);
+        // res.redirect(302, `${Canva.CANVA_BASE_URL}/apps/configured?${params}`);
+        res.json({response: `${Canva.CANVA_BASE_URL}/apps/configured?${params}`})
       };
 
       // Get the nonce and expiry time stored in the cookie.
@@ -69,17 +49,19 @@ export default class Canva {
       const queryNonce = req.query.nonce?.toString(); 
 
       // After reading the cookie, clear it. This forces abandoned auth flows to be restarted.
-      res.clearCookie("nonce");
+      // res.clearCookie("nonce");
 
       let cookieNonce: string = "";
       let expiry: number = 0;
-
       try {
         [cookieNonce, expiry] = JSON.parse(cookieNonceAndExpiry);
       } catch (e) {
+
+        console.log("Error en mi try/catch", e);
         // If the nonce can't be parsed, assume something has been compromised and exit.
         return failureResponse();
       }
+
 
       // If the nonces are empty, exit the authentication flow.
       if (
@@ -120,8 +102,8 @@ export default class Canva {
       });
 
       // Redirect the user back to Canva
-      res.json({response: `${Canva.CANVA_BASE_URL}/apps/configured?${params}`})
-      // res.redirect(302, `${Canva.CANVA_BASE_URL}/apps/configured?${params}`);
+      res.redirect(302, `${Canva.CANVA_BASE_URL}/apps/configured?${params}`);
+      // res.json({response: `${Canva.CANVA_BASE_URL}/apps/configured?${params}`})
 
     } catch (error) {
       next(error);
@@ -134,7 +116,7 @@ export default class Canva {
    * @param str The string to check
    * @returns true if the string is nullish or empty, false otherwise
    */
-  private isEmpty(str?: string): boolean {
+  isEmpty(str?: string): boolean {
     return str == null || str.length == 0;
   }
 
